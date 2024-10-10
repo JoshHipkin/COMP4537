90ae4466a730bcae3f813eea6d18c92a516fa8d1
Josh Hipkin <jhipkin@my.bcit.ca>
2024-10-09 20:53:21 -0700

    fixed typo

--- Lab5/server.js
+++ Lab5/server.js
@@ -33,7 +33,7 @@ class Server {
 
   handleRequest(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Origin", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-requested-with");
 
     if (req.method === "OPTIONS") {
